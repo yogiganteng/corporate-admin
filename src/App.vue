75223567fdfdbb03d47f3212ceb08dc869e649b2
yogimardilah <yogimardilah19@gmail.com>
2021-10-15 14:39:25 +0700

    login logout dasboard

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link :to="{name: 'login'}" class="navbar-brand">LARAVEL PASSPORT + VUE</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link :to="{name: 'register'}" class="nav-link" href="">REGISTER</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name: 'login'}" class="nav-link" href="#">LOGIN</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: lightgray!important;
+  }
 </style>
